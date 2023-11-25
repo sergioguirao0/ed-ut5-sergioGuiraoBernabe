df322fe7bd1e6ae472ecacd15b2cd3f7c3c72c4d
sergioguirao0 <sergioguiraovg1>
2023-11-25 16:46:00 +0100

    Primera versión

---
+++ src/Program/Fibonacci.java
@@ -0,0 +1,55 @@
+package Program;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        // Se llama al método que ejecuta el programa
+        mostrarPrograma();
+    }
+
+    // Función recursiva que calcula un término de la serie de Fibonacci
+    static int serieFibonacci(int termino){
+        int actual; // Creo la variable actual para almacenar el valor de ese término de la serie fibonacci
+
+        /* El 1 será el caso base de la serie fibonacci, a partir de ahí, la suma de los dos últimos términos
+           es el siguiente. En caso de que termino sea 1, se almacenará ese valor. */
+        if(termino == 0 || termino == 1){
+            actual = 1;
+        }
+        else{
+            // El valor enésimo es igual a la suma de los dos valores anteriores
+            actual = serieFibonacci(termino - 1) + serieFibonacci(termino - 2);
+        }
+
+        return actual;
+    }
+
+    // Método para mostrar el programa desde main
+    static void mostrarPrograma(){
+        int cantidad;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("SERIE DE FIBONACCI");
+        System.out.println("------------------");
+        System.out.print("Indica la cantidad de terminos que quieres mostrar: ");
+        cantidad = scanner.nextInt();
+
+        // Con el condicional if/else se muestra un mensaje de error si la cantidad es menor que 1
+        if(cantidad > 0){
+            // Utilizo un bucle for para poder mostrar por pantalla cada uno de los valores
+            // Si no se utilizara el bucle for, mostraría solo el último valor.
+
+            for(int i = 0; i < cantidad; i++){
+                int valor = serieFibonacci(i);
+                System.out.print(valor + " ");
+            }
+        }
+
+        else{
+            System.out.println("ERROR!: La cantidad tiene que ser mayor o igual que 0");
+        }
+    }
+}
+
---
+++ src/Program/NumerosAleatorios.java
@@ -0,0 +1,103 @@
+package Program;
+import java.util.Scanner;
+
+public class NumerosAleatorios {
+    public static void main(String[] args) {
+        // Se llama a la función mostrar menú. Las funciones de generar aleatorios están dentro de esta función.
+        mostrarMenu();
+    }
+
+    // Función con parámetros mínimo, máximo y cantidad de números aleatorios entre esos dos valores.
+    static void generarAleatorios(int min, int max, int cant){
+        int aleatorio;
+        for(int i = 1; i <= cant; i++){
+            /* Utilizo la función random para crear números aleatorios entre 0 y 1. Estos números se multiplican por
+               la diferencia entre max y min para que los números estén entre 0 y el valor de la resta de max y min.
+               Con esto nos quedarían números aleatorios entre 0 y (max - min). Con round se redondea para que pueda
+               salir el máximo y con (int) se convierten a enteros. Luego se le suma el mínimo para que queden entre
+               el mínimo y el máximo. Si no se utilizara round, el número que se indica como máximo no aparecería
+               nunca entre las posibilidades
+             */
+            aleatorio = min + (int)Math.round(Math.random() * (max - min));
+            System.out.println(aleatorio);
+        }
+    }
+
+    // Función con parámetro cantidad que genera números aleatorios entre 0 y 1.
+    static void generarAleatorios(int cant){
+        double aleatorio;
+        for(int i = 1; i <= cant; i++){
+            // La función Math.random crea siempre números aleatorios entre 0 y 1
+            aleatorio = Math.random();
+            System.out.println(aleatorio);
+        }
+    }
+
+    // Función para mostrar el menú desde donde se llamarán a las otras funciones creadas
+    static void mostrarMenu(){
+        // Declaración de variables del método
+        int opcion;
+        int minimo;
+        int maximo;
+        int cantidadAleatorios;
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Información inicial
+        System.out.println("GENERADOR DE NÚMEROS ALEATORIOS");
+        System.out.println("-------------------------------");
+
+        // Bucle do/while para realizar el menú mientras no se indique la opción de salir del programa
+        do{
+            // Información del menú
+            System.out.println("Menú: ");
+            System.out.println("1. Genera números aleatorios eligiendo el mínimo, el máximo y la cantidad de números.");
+            System.out.println("2. Genera números aleatorios entre 0 y 1, eligiendo la cantidad.");
+            System.out.println("3. Salir del programa.");
+            System.out.print("\nElige una opción: ");
+
+            opcion = scanner.nextInt();
+
+            // Realizo un condicional switch para desarrollar las distintas opciones
+            switch(opcion){
+                case 1:
+                    // En la opción 1 se solicitan el mínimo, máximo y cantidad de aleatorios que se quieren ver.
+                    System.out.print("Indica el número mínimo: ");
+                    minimo = scanner.nextInt();
+                    System.out.print("Indica el número máximo: ");
+                    maximo = scanner.nextInt();
+                    System.out.print("Indica la cantidad de aleatorios que quieres generar: ");
+                    cantidadAleatorios = scanner.nextInt();
+
+                    // Se llama al método creado anteriormente con los 3 parámetros
+                    generarAleatorios(minimo, maximo, cantidadAleatorios);
+
+                    System.out.print("\n");
+
+                    break;
+
+                case 2:
+                    // En la opción 2 se solicita la cantidad de aleatorios
+                    System.out.print("Indica la cantidad de aleatorios que quieres generar: ");
+                    cantidadAleatorios = scanner.nextInt();
+
+                    // Se llama al método creado anteriormente con 1 parámetro
+                    generarAleatorios(cantidadAleatorios);
+
+                    System.out.print("\n");
+
+                    break;
+
+                case 3:
+                    // La opción 3 se utilizará para finalizar el programa
+                    System.out.println("Saliendo del programa. Gracias!");
+
+                    break;
+
+                default:
+                    // Mensaje en caso de opción no válida
+                    System.out.println("Opción no válida");
+            }
+        }while(opcion != 3);
+    }
+}
